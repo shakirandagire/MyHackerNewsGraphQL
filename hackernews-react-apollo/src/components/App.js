344f1f7f856c2f478051f05d0c93d11e96d24c44
shakirandagire <ndagire.shakira@gmail.com>
2019-01-04 06:18:40 +0300

    Add create and view links functionality

---
+++ hackernews-react-apollo/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import CreateLink from './CreateLink';
+
+class App extends Component {
+  render() {
+    return <CreateLink />
+  }
+}
+
+export default App
